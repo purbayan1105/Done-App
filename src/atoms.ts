26ab3171799eee700e412543d3084ff6f85e5790
purbayan1105 <purbayan.kol@gmail.com>
2024-06-10 01:25:38 +0530

    second commit- theme default dark

--- src/atoms.ts
+++ src/atoms.ts
@@ -2,6 +2,6 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { ObjProps } from "./components/TodoForm";
 
-export const darkAtom = atomWithStorage("dark-mode-toggle", false);
+export const darkAtom = atomWithStorage("dark-mode-toggle", true);
 export const listAtom = atomWithStorage<ObjProps[]>("my-to-do-list", []);
 export const inputAtom = atom("");
