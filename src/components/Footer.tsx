02fab2513a41d6fb13f494dcfcdf518cf107d4f8
purbayan1105 <purbayan.kol@gmail.com>
2024-06-10 15:21:23 +0530

    links added for git and drive

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,30 @@
+import Link from "next/link";
+import { FaGithub, FaGoogleDrive } from "react-icons/fa";
+
 const Footer = () => {
   return (
     <>
-      <div className="fixed bottom-0 w-full flex justify-center items-end py-5 dark:bg-[#14142e] shadow-2xl shadow-purple-500 bg-[#fafff4]">
-        <div className="flex items-center">
-          <div className=" text-sm lg:text-lg text-gray-400">
-            All Copyrights Reserved | ©{" "}
-          </div>
+      <div className="fixed bottom-0 w-full  py-5 dark:bg-[#14142e] shadow-2xl shadow-purple-500 bg-[#fafff4] px-5">
+        <div className="grid grid-cols-6">
+          <div className="flex justify-end items-center col-span-5 md:col-span-4">
+            <div className=" text-sm lg:text-lg text-gray-400">
+              All Copyrights Reserved | ©{" "}
+            </div>
 
-          <img src="/sign2.png" alt="" className="w-16 lg:w-32 mr-[25px]" />
+            <img src="/sign2.png" alt="" className="w-16 lg:w-32 mr-[25px]" />
+          </div>
+          <div className="flex md:justify-end  items-center gap-5 col-span-1">
+            <div className="">
+              <Link href="https://github.com/purbayan1105/Done-App">
+                <FaGithub className="" size={20} />
+              </Link>
+            </div>
+            <div className="">
+              <Link href="https://drive.google.com/drive/folders/1AGxTsZI0X7DdtaX2uy5Ua7ZU-XIyaJJo">
+                <FaGoogleDrive className="" size={20} />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </>
